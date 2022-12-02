b8b5755c613e1b8189066007f61bddc0fad0ddd7
Marcus Johansson <marcus.johansson@zenseact.com>
2022-12-02 07:07:31 +0100

    Remove code in day2 part 2 not used

--- src/day2.py
+++ src/day2.py
@@ -12,16 +12,6 @@ SCORE = {
     "C X": 2,
 }
 
-def _score_win(opponents:int, mine:int):
-    if opponents == mine:
-        return 3 # draw
-    if mine - opponents == 1: # winning move is exactly 1 step removed
-        return 6
-    if mine -opponents == -2: # wrap around
-        return 6
-    return 0
-
-
 def part2():
     score = 0
     for line in utils.read_inputs(2):
