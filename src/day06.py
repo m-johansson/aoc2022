781eb7f70045c7b6b298ae9c252402db88b21cb1
Marcus Johansson <marcus.johansson@zenseact.com>
2022-12-06 07:38:13 +0100

    Black formatting

--- src/day06.py
+++ src/day06.py
@@ -2,17 +2,19 @@ from . import utils
 
 WINDOW_SIZE = 14
 
+
 def sliding_window(sequence):
     for i in range(len(sequence) - WINDOW_SIZE + 1):
-        yield (i+WINDOW_SIZE, sequence[i: i + WINDOW_SIZE])
+        yield (i + WINDOW_SIZE, sequence[i : i + WINDOW_SIZE])
+
 
 def main(signal):
-    windows = ((index, set(window) )for index, window in sliding_window(list(signal)))
+    windows = ((index, set(window)) for index, window in sliding_window(list(signal)))
     for index, window in windows:
         if len(window) == WINDOW_SIZE:
             return index
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     signal = utils.read_inputs_as_single_string(6)
     print(main(signal))
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import pathlib
 
-def read_inputs_as_single_string(day:int):
+
+def read_inputs_as_single_string(day: int):
     filepath = pathlib.Path(__file__).parent.parent / f"inputs/day{day}.txt"
     with open(filepath) as fid:
         return fid.read()
