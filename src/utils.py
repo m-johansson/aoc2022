c0b71e3cc4c9d4a183ec81150c3b03710e0d9547
Marcus Johansson <marcus.johansson@zenseact.com>
2022-12-06 07:33:34 +0100

    Day06 both parts

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,10 @@
 import pathlib
 
+def read_inputs_as_single_string(day:int):
+    filepath = pathlib.Path(__file__).parent.parent / f"inputs/day{day}.txt"
+    with open(filepath) as fid:
+        return fid.read()
+
 
 def read_inputs(day: int):
     filepath = pathlib.Path(__file__).parent.parent / f"inputs/day{day}.txt"
