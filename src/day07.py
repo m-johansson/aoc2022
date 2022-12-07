0c67970cb0a41edd64b09ed188c7a15888ef9a51
Marcus Johansson <marcus.johansson@zenseact.com>
2022-12-07 09:13:00 +0100

    day07

--- src/day07.py
+++ src/day07.py
@@ -3,34 +3,40 @@ import typing
 
 from . import utils
 
-class File:
 
-    def __init__(self, path:str, size:int) -> None:
+class File:
+    def __init__(self, path: str, size: int) -> None:
         self.path = path
         self.size = size
 
-class Dir:
+    def __repr__(self) -> str:
+        return f"{self.path}:{self.size}"
 
-    def __init__(self, path:str) -> None:
+
+class Dir:
+    def __init__(self, path: str) -> None:
         self.path = path
         self.size = 0
         self.files = []
 
-    def add_file(self, file:File):
+    def add_file(self, file: File):
         if self.path in file.path:
             self.size += file.size
             self.files.append(file)
 
+    def list_files(self):
+        for file in self.files:
+            print(file)
+
     def __repr__(self) -> str:
-        return self.path + " " + str(self.size) 
+        return self.path + " " + str(self.size)
 
 
 class FileSystem:
-
     def __init__(self) -> None:
         self.cwd = pathlib.Path("/")
 
-    def cd(self, arg:str):
+    def cd(self, arg: str):
         if arg == "..":
             self.cwd = self.cwd.parent
         else:
@@ -38,13 +44,34 @@ class FileSystem:
 
     def get_cwd(self) -> str:
         return str(self.cwd)
-        
 
+    def get_absolute_path(self, path: str) -> str:
+        return str(self.cwd / path)
+
+
+def part1(dirs) -> int:
+    limit = 100000
+    small_dirs = [d for d in dirs if d.size < limit]
+    answer = 0
+    for d in small_dirs:
+        answer += d.size
+    return answer
+
+
+def part2(dirs) -> int:
+    total_size = 70000000
+    target = 30000000
+    root_dir = dirs[0]
+    free_space = total_size - root_dir.size
+    delete_limit = target - free_space
+    candidates = [d for d in dirs if d.size >= delete_limit]
+    candidates.sort(key=lambda x: x.size)
+    return candidates[0].size
 
 
 def main(data):
     fs = FileSystem()
-    dirs = []
+    dirs = [Dir("/")]
     files = []
     while True:
         try:
@@ -55,33 +82,20 @@ def main(data):
             cd_arg = line.split()[-1]
             fs.cd(cd_arg)
         elif line.startswith("$ ls"):
-            pass #this is a no action really
+            pass  # this is a no action really
         elif line.startswith("dir"):
             dirname = line.split()[-1]
-            dirs.append(Dir(fs.get_cwd()+f"/{dirname}"))
-        else: #this is a file, yehaw!
+            dirs.append(Dir(fs.get_absolute_path(dirname)))
+        else:  # this is a file, yehaw!
             size = int(line.split()[0])
             name = line.split()[1]
-            files.append(File(fs.get_cwd() + f"/{name}", size))
-
-
+            files.append(File(fs.get_absolute_path(name), size))
     for file in files:
         for d in dirs:
             d.add_file(file)
-    limit = 100000
-    small_dirs = [d for d in dirs if d.size < limit]
-    answer = 0
-    for d in small_dirs:
-        answer += d.size
-    print(answer)
-    print(dirs)
-    return answer
+    print(part2(dirs))
 
 
-
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     lines = utils.read_inputs(7)
-    next(lines) # throw away "$ cd /" as we're already having cwd /
     main(lines)
