e69b2a7a60abadaa64cf573ee0b7069e3c8e229f
Marcus Johansson <marcus.johansson@zenseact.com>
2022-12-09 18:41:21 +0100

    Black format day08

--- src/day08.py
+++ src/day08.py
@@ -15,14 +15,16 @@ def is_taller(height, iterable):
 def visible_trees(height, iterable):
     tree_count = 0
     for h in iterable:
-        tree_count+=1
+        tree_count += 1
         if h >= height:
             return tree_count
-    return tree_count # this is reached on the edge
+    return tree_count  # this is reached on the edge
+
 
 def get_col(data, col_index):
     col = [row[col_index] for row in data]
     return col
+
 
 def part1(trees):
     count = 0
@@ -40,18 +42,20 @@ def part1(trees):
                 count += 1
     return count
 
+
 def part2(trees):
     max_score = 0
     for row_index, row in enumerate(trees):
         for col_index, height in enumerate(row):
             left = visible_trees(height, reversed(row[:col_index]))
-            right = visible_trees(height, row[col_index+1:])
-            col = get_col(trees,col_index)
+            right = visible_trees(height, row[col_index + 1 :])
+            col = get_col(trees, col_index)
             up = visible_trees(height, reversed(col[:row_index]))
-            down = visible_trees(height, col[row_index+1:])
+            down = visible_trees(height, col[row_index + 1 :])
             score = left * right * up * down
             max_score = score if score > max_score else max_score
     return max_score
+
 
 def main(data):
     trees = build_matrix(data)
